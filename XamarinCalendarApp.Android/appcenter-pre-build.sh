aaad7d6f3cc4e439c08818955699f6e119e2d4aa
Ponnu Mittathani <ponnumittathani264@gmail.com>
2022-07-15 00:46:31 +0530

    script updates

--- XamarinCalendarApp.Android/appcenter-pre-build.sh
+++ XamarinCalendarApp.Android/appcenter-pre-build.sh
@@ -3,7 +3,7 @@
 # Example: Change bundle name of an iOS app for non-production
 if [ "$APPCENTER_BRANCH" != "main" ];
 then
-    MANIFEST_PATH="$APPCENTER_SOURCE_DIRECTORY/XamarinCalendarApp/Properties/AndroidManifest.xml"
-VERSION_CODE=$(7)
+    MANIFEST_PATH="$APPCENTER_SOURCE_DIRECTORY/XamarinCalendarApp.Android/Properties/AndroidManifest.xml"
+VERSION_CODE=$(10)
 sed -i "" 's/android:versionCode="[^"]*"/android:versionCode="'$VERSION_CODE'"/' $MANIFEST_PATH
 fi
