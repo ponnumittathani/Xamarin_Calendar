080a6ef2e674cb7a31d1a8e7c76819c0ddd69ae7
Ponnu Mittathani <ponnumittathani264@gmail.com>
2022-07-15 01:15:20 +0530

    script updates

--- XamarinCalendarApp.Android/appcenter-pre-build.sh
+++ XamarinCalendarApp.Android/appcenter-pre-build.sh
@@ -4,6 +4,6 @@
 if [ "$APPCENTER_BRANCH" != "main" ];
 then
     MANIFEST_PATH="$APPCENTER_SOURCE_DIRECTORY/XamarinCalendarApp.Android/Properties/AndroidManifest.xml"
-VERSION_CODE=$((VERSION_CODE_SHIFT + APPCENTER_BUILD_ID))
+VERSION_CODE=$((10 + APPCENTER_BUILD_ID))
 sed -i "" 's/android:versionCode="[^"]*"/android:versionCode="'$VERSION_CODE'"/' $MANIFEST_PATH
 fi
