347319c958ad24592fcfef911cd0b070147bad39
Ponnu Mittathani <ponnumittathani264@gmail.com>
2022-07-15 01:24:15 +0530

    added scripts for ios

---
+++ XamarinCalendarApp.iOS/appcenter-pre-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Example: Change bundle name of an iOS app for non-production
+if [ "$APPCENTER_BRANCH" != "main" ];
+then
+    VERSION_CODE=$((3 + APPCENTER_BUILD_ID))
+plutil -replace CFBundleVersion -string "$VERSION_CODE"
+$APPCENTER_SOURCE_DIRECTORY/PXamarinCalendarApp.iOS/Info.plist
+fi
